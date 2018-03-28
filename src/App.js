fe9dc655fe5df996b0fbcde5c77ca9661dc72b5c
wences627 <hugo@localhost.localdomain>
2018-03-28 12:50:27 -0400

    cambio de app.js

--- src/App.js
+++ src/App.js
@@ -1,18 +1,18 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Container from './components/Container'
+
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Container  size= {0.3} color="black">
+          <p style={{color: 'white'}}> hola que hace </p>
+        </Container>
+        <Container size={0.6} color="white" />
+        <Container size={0.1} color= "black" />
       </div>
     );
   }
