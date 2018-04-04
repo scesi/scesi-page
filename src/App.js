b654299abf37ff08a1b84761354c9dd91d87f175
wences627 <wences628@gmail.com>
2018-04-04 19:42:22 -0400

    implementacion del logo en el header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,15 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 import Container from './components/Container'
-
+import logo from './scesi.png';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Container  size= {0.3} color="black">
-          <p style={{color: 'white'}}> hola que hace </p>
+          <img src={logo} className="App-logo" alt="logo" />
+          <p style={{color: 'white'}}>happy hacking</p>
         </Container>
         <Container size={0.6} color="white" />
         <Container size={0.1} color= "black" />
